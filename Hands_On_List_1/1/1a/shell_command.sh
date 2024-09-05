6034673e1c6e7609357b5d41b6f7587a705b4d39
Jerome Joseph <jeromejoseph5813@gmail.com>
2024-09-06 02:18:47 +0530

    Adjusted gitignore, fixed 1a output - sh

--- Hands_On_List_1/1/1a/shell_command.sh
+++ Hands_On_List_1/1/1a/shell_command.sh
@@ -5,5 +5,5 @@
 # files created 
 
 ❯ ls -ali file1 file_s_1                                                                                                             ─╯
-13501087 -rw-r--r-- 2 jerome jerome 28 Aug 30 19:19 file1
+13501087 -rw-r--r-- 1 jerome jerome 28 Aug 30 19:19 file1
 13501573 lrwxrwxrwx 1 jerome jerome  5 Aug 30 19:29 file_s_1 -> file1
